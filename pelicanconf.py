3275f1a770dd5a9a8704b7e94cb804574ad69fb7
shane doucette <shaniber@gmail.com>
2025-04-27 12:19:46 -0600

    Updating pelican config files

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,21 +2,21 @@
 # -*- coding: utf-8 -*- #
 
 AUTHOR = u'shane doucette'
-SITENAME = u'recipe box'
-SITEURL = 'http://tauntedechoes.com/rb'
+SITENAME = u'shane\'s recipe box'
+SITEURL = 'https://shaniber.net/recipes'
 
 TIMEZONE = 'America/Regina'
 
 DEFAULT_LANG = u'en'
 
 # Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+#LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+#          ('Python.org', 'http://python.org'),
+#          ('Jinja2', 'http://jinja.pocoo.org'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
